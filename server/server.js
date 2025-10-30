b984ef5db36df1aa890e4889a1faf6166f7f3ca0
abhikmahajan <abhikmahajan01@gmail.com>
2025-10-30 09:26:28 +0530

    Server changed a bit

--- server/server.js
+++ server/server.js
@@ -12,10 +12,7 @@ const cookieParser = require("cookie-parser");
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: 'job-board-app-sable.vercel.app',
-  credentials: true
-}));
+app.use(cors());
 app.use(cookieParser());  
 
 // Routes
@@ -28,6 +25,10 @@ app.use('/api/recruiter', recruiterRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.error('MongoDB Connection Error:', err));
+
+  app.get('/',(req,res) =>
+res.send("Server is live!")
+)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
